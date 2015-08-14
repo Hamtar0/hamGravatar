77e9a6be27b92b7d02ac419c0064397f0cbbf611
Damien Guillot <damien@guillot.nu>
2015-08-14 18:08:14 +0200

    Pluxml 5.4
    Affichage des Gravatars dans le tableau des utilisateurs
    (admin/parametres_users.php)

--- hamGravatar.php
+++ hamGravatar.php
@@ -5,13 +5,13 @@ class hamGravatar extends plxPlugin {
 	* Constructeur de la classe
 	*
 	* @return	null
-	* @author	StÈphane F.  
-	**/	
+	* @author	StÈphane F.
+	**/
 	public function __construct($default_lang) {
 
 		# Appel du constructeur de la classe plxPlugin (obligatoire)
 		parent::__construct($default_lang);
-		
+
 		# droits pour accéder ‡ la page config.php du plugin
 		$this->setConfigProfil(PROFIL_ADMIN);
 		$this->setAdminProfil(PROFIL_ADMIN);
@@ -23,9 +23,9 @@ class hamGravatar extends plxPlugin {
 		$this->addHook('AdminUserTop', 'AdminUserTop');
 		$this->addHook('ThemeEndBody', 'ThemeEndBody');
 		$this->addHook('ThemeEndHead', 'ThemeEndHead');
-		
+
 	}
-	
+
 	public function AdminUsersFoot() {?>
 		<script type="text/javascript">
 		/* <![CDATA[ */
@@ -34,15 +34,15 @@ class hamGravatar extends plxPlugin {
 		}
 		/* ]]> */
 		</script>
-		
+
 		<?php echo '<?php $table_profils = json_encode($plxAdmin->aUsers); ?>'; ?>
-		
+
 		<script type="text/javascript" src="<?php echo PLX_PLUGINS ?>hamGravatar/js/md5.js"></script>
 		<style type="text/css">th.gravatar {width:55px;}</style>
 		<script type="text/javascript">
 		/* <![CDATA[ */
 		$(document).ready(function(){
-					
+
 			function calculateGravatar(emailAddress) {
 				var grav ='';
 				var email = emailAddress.toLowerCase().trim();
@@ -53,27 +53,27 @@ class hamGravatar extends plxPlugin {
 				return grav;
 			}
 
-			$('<th></th>').addClass('title gravatar').text('Gravatar').insertBefore('.table th:eq(0)');
+			$('<th></th>').addClass('title gravatar').text('Gravatar').insertBefore('#users-table th:eq(0)');
 
 			var table_profils = <?php echo '<?php echo $table_profils; ?>' ?>;
 
 			$.each(table_profils, function(i, item) {
-						
+
 				var gravatarimg = calculateGravatar(item.email);
 
-				$('<td></td>').html('<a title="Envoyer un email" href="mailto:'+item.email+'" target="_top"><img src="'+gravatarimg+'" /></a>').insertBefore('.table tbody tr:eq('+(i-1)+') td:eq(0)');
+				$('<td></td>').html('<a title="Envoyer un email" href="mailto:'+item.email+'" target="_top"><img src="'+gravatarimg+'" /></a>').insertBefore('#users-table tbody tr:eq('+(i-1)+') td:eq(0)');
 
 			});
-					
-			$('<td></td>').insertBefore('.table tbody tr:last td:eq(0)');
-	
+
+			$('<td></td>').insertBefore('#users-table tbody tr:last td:eq(0)');
+
 		});
 		/* ]]> */
 		</script>
 
 		<?php
 	}
-	
+
 	public function AdminUserTop() {?>
 		<script type="text/javascript">
 		/* <![CDATA[ */
@@ -82,12 +82,12 @@ class hamGravatar extends plxPlugin {
 		}
 		/* ]]> */
 		</script>
-		
+
 		<script type="text/javascript" src="<?php echo PLX_PLUGINS ?>hamGravatar/js/md5.js"></script>
 		<script type="text/javascript">
 		/* <![CDATA[ */
 		$(document).ready(function(){
-					
+
 			function calculateGravatar(emailAddress) {
 				var grav ='';
 				var email = emailAddress.toLowerCase().trim();
@@ -97,26 +97,26 @@ class hamGravatar extends plxPlugin {
 				grav = 'http://www.gravatar.com/avatar/'+hash+'?s=150&r='+rated+'&d='+defaut;
 				return grav;
 			}
-					
+
 			var email = $('#id_email');
-					
+
 			var gravatarimg = calculateGravatar( email.val() );
-					
+
 			$('#content').css('background', 'url("' + gravatarimg + '") no-repeat top 20px right 20px ');
-					
+
 			email.blur(function() {
 				var gravatarimg = calculateGravatar( email.val() );
 				$('#content').css('background', 'url("' + gravatarimg + '") no-repeat top 20px right 20px ');
 			});
-					
-	
+
+
 		});
 		/* ]]> */
 		</script>
 
 		<?php
 	}
-	
+
 	public function AdminProfilTop() {?>
 		<script type="text/javascript">
 		/* <![CDATA[ */
@@ -125,12 +125,12 @@ class hamGravatar extends plxPlugin {
 		}
 		/* ]]> */
 		</script>
-		
+
 		<script type="text/javascript" src="<?php echo PLX_PLUGINS ?>hamGravatar/js/md5.js"></script>
 		<script type="text/javascript">
 		/* <![CDATA[ */
 		$(document).ready(function(){
-				
+
 			function calculateGravatar(emailAddress) {
 				var grav ='';
 				var email = emailAddress.toLowerCase().trim();
@@ -140,18 +140,18 @@ class hamGravatar extends plxPlugin {
 				grav = 'http://www.gravatar.com/avatar/'+hash+'?s=150&r='+rated+'&d='+defaut;
 				return grav;
 			}
-				
+
 			var email = $('#id_email');
-				
+
 			var gravatarimg = calculateGravatar( email.val() );
-				
+
 			$('#form_profil').css('background', 'url("' + gravatarimg + '") no-repeat top 20px right 20px ');
-				
+
 			email.blur(function() {
 				var gravatarimg = calculateGravatar( email.val() );
 				$('#form_profil').css('background', 'url("' + gravatarimg + '") no-repeat top 20px right 20px ');
 			});
-				
+
 
 		});
 		/* ]]> */
@@ -159,16 +159,16 @@ class hamGravatar extends plxPlugin {
 
 		<?php
 	}
-	
+
 	public function ThemeEndBody() {
 		if ($this->getParam('activate_textarea') =='ok') {
 	?>
-		
+
 		<script type="text/javascript" src="<?php echo PLX_PLUGINS ?>hamGravatar/js/md5.js"></script>
 		<script type="text/javascript">
 		/* <![CDATA[ */
 		$(document).ready(function(){
-					
+
 			function calculateGravatar(emailAddress) {
 				var grav ='';
 				var email = emailAddress.toLowerCase().trim();
@@ -178,28 +178,28 @@ class hamGravatar extends plxPlugin {
 				grav = 'http://www.gravatar.com/avatar/'+hash+'?s=50&r='+rated+'&d='+defaut;
 				return grav;
 			}
-					
+
 			var email = $('input[name="mail"]');
-			
+
 			console.log(email.val());
-					
+
 			var gravatarimg = calculateGravatar( email.val() );
-					
+
 			$('textarea[name="content"]').css('background', 'url("' + gravatarimg + '") no-repeat bottom 5px right 5px ');
-					
+
 			email.blur(function() {
 				var gravatarimg = calculateGravatar( email.val() );
 				$('textarea[name="content"]').css('background', 'url("' + gravatarimg + '") no-repeat bottom 5px right 5px ');
 			});
-					
-	
+
+
 		});
 		/* ]]> */
 		</script>
 
 		<?php }
 	}
-	
+
 	public function ThemeEndHead() {
 		if ($this->getParam('activate_textarea') =='ok') {
 	?>
@@ -213,7 +213,7 @@ class hamGravatar extends plxPlugin {
 
 		<?php }
 	}
-	
+
 	public function hamGravatar () {
 		$hamGravatar_size = $this->getParam('hamGravatar_size');
 		$hamGravatar_defaut = $this->getParam('hamGravatar_defaut');
@@ -221,7 +221,7 @@ class hamGravatar extends plxPlugin {
 		$hamGravatar_rated = $this->getParam('hamGravatar_rated');
 		$plxShow = plxShow::getInstance();
 		$type = $plxShow->plxMotor->plxRecord_coms->f('type');
-		
+
 		if($type=='admin') {
 			$email = $plxShow->plxMotor->plxRecord_coms->f('mail');
 			if($email!='') $hamGravatar = $email;
@@ -232,15 +232,15 @@ class hamGravatar extends plxPlugin {
 		} else {
 			$hamGravatar = plxUtils::strCheck($plxShow->plxMotor->plxRecord_coms->f('mail'));
 		}
-	        
+
 		if($hamGravatar_defaut == 'url') {
 			$hamGravatar_d = urlencode($hamGravatar_defaut_url);
 		} else {
 			$hamGravatar_d = $hamGravatar_defaut;
 		}
-	        
+
 		$hamGravatar_mail = md5( strtolower( trim($hamGravatar)));
-		
+
 		echo 'http://www.gravatar.com/avatar.php?gravatar_id='.$hamGravatar_mail.'&amp;d='.$hamGravatar_d.'&amp;s='.$hamGravatar_size.'&amp;r='.$hamGravatar_rated;
 	}
 
